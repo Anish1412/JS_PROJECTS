7d4cac24a9a7e43cb2ae615885ccee72282f4f59
Anish1412 <anishpatil1412@gmail.com>
2023-07-25 07:34:24 +0530

    Todo list

--- JS/TODO_List.js
+++ JS/TODO_List.js
@@ -1,32 +1,55 @@
+// "Tasks" LocalStorage
 let tasks = JSON.parse(localStorage.getItem("Tasks"));
 let arrTasks = [];
 arrTasks.push(tasks);
 let newArray = arrTasks.flat(Infinity);
 
+// "BooleanValue" LocalStorage
+let boolValue = JSON.parse(localStorage.getItem("BooleanValue"));
+let checkboxValue = [];
+checkboxValue.push(boolValue);
+let newCheckboxValue = checkboxValue.flat(Infinity);
+
 function retainTask(id) {
   let li = document.querySelectorAll("li")[id].innerText;
-  document.querySelectorAll("li")[id].innerHTML = li+"<input type='checkbox' style='margin-left:10px;"+
-  "height:18px; width:18px' onclick='cancelTask("+id+")'>"+
-  "<button type='button' class='btn2' onclick='updateTask("+id+")'>Update Task</button>"+
-  "<button type='button' class='btn1' onclick='removeTask("+id+")'>Delete Task</button>";
+  document.querySelectorAll("li")[id].innerHTML =
+    li +
+    "<input type='checkbox' style='margin-left:10px;" +
+    "height:18px; width:18px' onclick='cancelTask(" +
+    id +
+    ")'>" +
+    "<button type='button' class='btn2' onclick='updateTask(" +
+    id +
+    ")'>Update Task</button>" +
+    "<button type='button' class='btn1' onclick='removeTask(" +
+    id +
+    ")'>Delete Task</button>";
   document.querySelectorAll("li")[id].style.opacity = "1";
-  console.log(li);
+  newCheckboxValue[id] = false;
+  localStorage.setItem("BooleanValue", JSON.stringify(newCheckboxValue));
 }
 
 function cancelTask(id) {
-  let li = document.querySelectorAll("#demo")[0].children[id].firstChild.textContent;
+  let li =
+    document.querySelectorAll("#demo")[0].children[id].firstChild.textContent;
   document.querySelectorAll("li")[id].innerHTML =
-    "<s>"+li+"<input type='checkbox' style='margin-left:10px; height:18px; width:18px' onclick='retainTask("+id+")'></s>";
+    "<s>" +
+    li +
+    "<input type='checkbox' style='margin-left:10px; height:18px; width:18px' id='clickButton' onclick='retainTask(" +
+    id +
+    ")' checked></s>";
   document.querySelectorAll("li")[id].style.opacity = "0.3";
-  document.querySelectorAll("input")[id].toggleAttribute("checked");
+  newCheckboxValue[id] = true;
+  localStorage.setItem("BooleanValue", JSON.stringify(newCheckboxValue));
 }
 
 function updateTask(id) {
   let newContent = prompt("Update Task : ", newArray[id]);
-  if(newContent){
+  if (newContent) {
     newArray[id] = newContent;
     localStorage.setItem("Tasks", JSON.stringify(newArray));
-    document.querySelectorAll("#demo")[0].children[id].firstChild.textContent = newContent;
+    document.querySelectorAll("#demo")[0].children[id].firstChild.textContent =
+      newContent;
   }
 }
 
@@ -70,6 +93,10 @@ let createTask = () => {
       li.appendChild(btn1);
       li.style = "margin:15px 15px 15px 0px; font-size:20px";
       document.getElementById("demo").append(li);
+
+      // ------ Boolean Value Array ------
+      newCheckboxValue.push(false);
+      localStorage.setItem("BooleanValue", JSON.stringify(newCheckboxValue));
     }
   }
 };
@@ -77,42 +104,58 @@ let createTask = () => {
 // ********* Displays the tasks of todo list from the localStorage *********
 for (let i = 0; i < tasks.length; i++) {
   let li = document.createElement("li");
-  let text = document.createTextNode(tasks[i]);
-  let input = document.createElement("input");
-  input.type = "checkbox";
-  input.style = "margin-left:10px; height:18px; width:18px";
-  input.onclick = () => {
-    cancelTask(i);
-  };
-  let btn = document.createElement("button");
-  btn.type = "button";
-  btn.className = "btn2";
-  btn.onclick = () => {
-    updateTask(i);
-  };
-  let btnText = document.createTextNode("Update Task");
-  btn.appendChild(btnText);
+  if (boolValue[i]) {
+    let s = document.createElement("s");
+    let text = document.createTextNode(tasks[i]);
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.style = "margin-left:10px; height:18px; width:18px";
+    input.checked = boolValue[i];
+    input.onclick = () => retainTask(i);
+    s.appendChild(text);
+    s.appendChild(input);
+    li.appendChild(s);
+    li.style = "margin:15px 15px 15px 0px; font-size:20px;opacity:0.3";
+    document.getElementById("demo").append(li);
+  } else {
+    let text = document.createTextNode(tasks[i]);
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    input.style = "margin-left:10px; height:18px; width:18px";
 
-  let btn1 = document.createElement("button");
-  btn1.type = "button";
-  btn1.className = "btn1";
-  btn1.onclick = () => {
-    removeTask(i);
-  };
-  let btnText1 = document.createTextNode("Delete Task");
-  btn1.appendChild(btnText1);
+    input.onclick = () => cancelTask(i);
+    let btn = document.createElement("button");
+    btn.type = "button";
+    btn.className = "btn2";
+    btn.onclick = () => {
+      updateTask(i);
+    };
+    let btnText = document.createTextNode("Update Task");
+    btn.appendChild(btnText);
 
-  li.appendChild(text);
-  li.appendChild(input);
-  li.appendChild(btn);
-  li.appendChild(btn1);
-  li.style = "margin:15px 15px 15px 0px; font-size:20px";
-  document.getElementById("demo").append(li);
+    let btn1 = document.createElement("button");
+    btn1.type = "button";
+    btn1.className = "btn1";
+    btn1.onclick = () => {
+      removeTask(i);
+    };
+    let btnText1 = document.createTextNode("Delete Task");
+    btn1.appendChild(btnText1);
+
+    li.appendChild(text);
+    li.appendChild(input);
+    li.appendChild(btn);
+    li.appendChild(btn1);
+    li.style = "margin:15px 15px 15px 0px; font-size:20px";
+    document.getElementById("demo").append(li);
+  }
 }
 
 // ********* Remove tasks from the todo list *********
 function removeTask(id) {
-  newArray.splice(id,1);
+  newArray.splice(id, 1);
+  newCheckboxValue.splice(id, 1);
   localStorage.setItem("Tasks", JSON.stringify(newArray));
+  localStorage.setItem("BooleanValue", JSON.stringify(newCheckboxValue));
   document.getElementById("demo").children[id].remove();
 }
